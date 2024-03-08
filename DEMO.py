44d951478d6ff92941ccb5b0ffd3aa17d7d9e660
TVAHMED <160422099+TVAHMED@users.noreply.github.com>
2024-03-08 14:36:43 +0500

    Update DEMO.py
    pkg update
    pkg upgrade 
    pkg install python
    pkg install git
    git clone https://github.com/TVAHMED/DEMO
    
    RUN
    cd DEMO
    ls
    python DEMO.py

--- DEMO.py
+++ DEMO.py
@@ -32,8 +32,8 @@ def logo():
     print(50*'-')
     print(f" {white}{green}[~]{white} Owner     : AHMED SHAR (ðŸ˜Ž")
     print(f" {white}{green}[~]{white} Facebook  : prince king hu yawr")
-    print(f" {white}{green}[~]{white} YouTube   : nhi pta (🕶️)
-    print(f" {white}{green}[~]{white} Tool      : FILE + RANDOM CLONE")
+    print(f" {white}{green}[~]{white} friend    : SOHAIL GAFOOR
+    print(f" {white}{green}[~]{white} Tool      :FILE + RANDOM CLONE")
     print(f" {white}{green}[~]{white} Github    : https://github.com/TVAHMED")
     print(f" {white}{green}[~]{white} Whatsapp  : 03277025198 ONLY for help ")
     print(f" {white}{green}[~]{white} Status    : {green}Free ")
